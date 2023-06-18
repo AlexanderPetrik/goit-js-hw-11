aa6cdbf3941157c5fba0879fd51d43d641e496e0
AlexanderPetrik <painkiller408@gmail.com>
2023-06-18 22:32:41 +0300

    js-hw-11

--- src/index.js
+++ src/index.js
@@ -1,1 +1,155 @@
 import axios from 'axios';
+import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// Ключ библиотеки axios
+const KEY = '37429759-66239b447aa4c13fe873ee915';
+
+const searchForm = document.getElementById('search-form');
+const gallery = document.querySelector('.gallery');
+
+let query = '';
+let page = 1;
+let simpleLightBox;
+const perPage = 40;
+
+// Делаем проверку на ошибки при запросе
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+axios.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    Notiflix.Notify.failure('Something went wrong. Please try again later.');
+    return Promise.reject(error);
+  },
+);
+
+async function fetchImages(query, page, perPage) {
+  const response = await axios.get(
+    `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
+  );
+  return response.data;
+};
+
+searchForm.addEventListener('submit', onSearchForm);
+
+function renderGallery(images) {
+  // Проверка наличя галереи перед вcтавкой данных
+  if (!gallery) {
+    return;
+  }
+
+  const markup = images
+    .map(image => {
+      const {
+        id,
+        largeImageURL,
+        webformatURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      } = image;
+      return `
+        <a class="gallery__link" href="${largeImageURL}">
+          <div class="gallery-item" id="${id}">
+            <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+            <div class="info">
+              <p class="info-item"><b>Likes</b>${likes}</p>
+              <p class="info-item"><b>Views</b>${views}</p>
+              <p class="info-item"><b>Comments</b>${comments}</p>
+              <p class="info-item"><b>Downloads</b>${downloads}</p>
+            </div>
+          </div>
+        </a>
+      `;
+    })
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', markup);
+
+  // Возможность прокручивать страницу на висоту 2 карточек галереи, после ее загрузки
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
+
+function onSearchForm(e) {
+  e.preventDefault();
+  page = 1;
+  query = e.currentTarget.elements.searchQuery.value.trim();
+  gallery.innerHTML = '';
+
+  if (query === '') {
+    Notiflix.Notify.failure(
+      'The search string cannot be empty. Please specify your search query.',
+    );
+    return;
+  }
+
+  fetchImages(query, page, perPage)
+    .then(data => {
+      if (data.totalHits === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.',
+        );
+      } else {
+        renderGallery(data.hits);
+        simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+      }
+    })
+    .catch(error => console.log(error))
+    .finally(() => {
+      searchForm.reset();
+    });
+}
+
+function onloadMore() {
+  page += 1;
+  // simpleLightBox.destroy();
+  // simpleLightBox.refresh();
+
+  fetchImages(query, page, perPage)
+    .then(data => {
+      renderGallery(data.hits);
+      simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+
+      const totalPages = Math.ceil(data.totalHits / perPage);
+
+      if (page > totalPages) {
+        Notiflix.Notify.failure(
+          "We're sorry, but you've reached the end of search results.",
+        );
+      }
+    })
+    .catch(error => console.log(error));
+}
+
+function checkIfEndOfPage() {
+  return (
+    // window.innerHeight + window.pageYOffset >=
+    window.innerHeight + window.scrollY >=
+    document.documentElement.scrollHeight
+  );
+}
+
+// Выполняеться, если пользователь дошел до конца страницы
+function showLoadMorePage() {
+  if (checkIfEndOfPage()) {
+    onloadMore();
+  }
+}
+
+// Добапвляем событие на скрол страницы, которая вызывает функцию showLoadMorePage
+window.addEventListener('scroll', showLoadMorePage);
+
